14ad5333761cc47cf00e5993c77b2fae4a67020c
Osmo Salomaa <otsaloma@iki.fi>
2015-05-23 21:52:20 +0300

    Return if pool no longer alive.

--- htl/http.py
+++ htl/http.py
@@ -87,12 +87,14 @@ class ConnectionPool:
 
     def put(self, url, connection):
         """Return `connection` to the pool of connections."""
+        if not self._alive: return
         key = self._get_key(url)
         self._queue[key].task_done()
         self._queue[key].put(connection)
 
     def reset(self, url):
         """Close and re-establish HTTP connection to `url`."""
+        if not self._alive: return
         connection = self.get(url)
         with htl.util.silent(Exception):
             connection.close()
@@ -101,6 +103,7 @@ class ConnectionPool:
     @htl.util.locked_method
     def terminate(self):
         """Close all connections and terminate."""
+        if not self._alive: return
         for key in self._queue:
             with htl.util.silent(queue.Empty):
                 while True:
@@ -170,7 +173,6 @@ def request_url(url, encoding=None, retry=1):
                   file=sys.stderr)
             raise # Exception
     finally:
-        if pool.is_alive():
-            pool.put(url, connection)
+        pool.put(url, connection)
     assert retry > 0
     return request_url(url, encoding, retry-1)
